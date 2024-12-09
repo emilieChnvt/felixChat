31f561397f4ca36dff39e796efae3480cc13af9f
Emilie <emilie.chnvt@gmail.com>
2024-12-09 19:06:14 +0100

    async/await added

--- js/main.js
+++ js/main.js
@@ -1,33 +1,37 @@
 let  token = null;
 
-let params ={
-    method:"POST",
-    headers:{
-        "Content-type":"application/json",
-        "Authorization": `Bearer ${token}`,
+async function login() {
+    let params ={
+        method:"POST",
+        headers:{
+            "Content-type":"application/json",
+            "Authorization": `Bearer ${token}`,
 
-    },
-    body: JSON.stringify({
-        username: 'pierre',
-        password: 'XqSn5rM35sQT',
-    })
+        },
+        body: JSON.stringify({
+            username: 'pierre',
+            password: 'XqSn5rM35sQT',
+        })
 
+    }
+
+
+
+    return await fetch('https://felix.esdlyon.dev/login', params)
+        .then(response => response.json())
+        .then(data =>{
+                console.log(data.token)
+                token = data.token
+                poseUneQuestionaIa()
+            }
+
+
+
+        )
 }
+login()
 
-
-
-fetch('https://felix.esdlyon.dev/login', params)
-    .then(response => response.json())
-    .then(data =>{
-            console.log(data.token)
-            token = data.token
-            poseUneQuestionaIa()
-        }
-
-
-
-)
-function poseUneQuestionaIa(){
+async function poseUneQuestionaIa(){
     let parameters ={
         method:"POST",
         headers:{
@@ -38,7 +42,7 @@ function poseUneQuestionaIa(){
             prompt: "bonjour, je m'appelle Emilie",
         } )
     }
-    fetch('https://felix.esdlyon.dev/ollama', parameters)
+    return await fetch('https://felix.esdlyon.dev/ollama', parameters)
         .then(response => response.json())
         .then(data =>{
             console.log(data)
