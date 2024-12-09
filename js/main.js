4795c2a37785653ef93bd4016306d0d1cbc4c1ca
Emilie <emilie.chnvt@gmail.com>
2024-12-09 17:52:44 +0100

    html added

--- js/main.js
+++ js/main.js
@@ -3,31 +3,38 @@ let  token = null;
 
 
 
-async function login(){
+async function login(username, password) {
+    console.log(username, password);
     let params ={
         method:"POST",
         headers:{
             'Content-type':"application/json",
         },
         body: JSON.stringify({
-            username: 'pierre',
-            password: 'XqSn5rM35sQT',
+            username: username,
+            password: password,
         } )
     }
 
     return await fetch('https://felix.esdlyon.dev/login', params)
         .then(response => response.json())
         .then(data =>{
-                console.log(data.token)
-                return data
-
+                return data.token
             }
 
         )
 
 }
 
+function displayLoginPage() {
 
+}
+
+if(!token){
+    displayLoginPage()
+}else{
+    displayLoginPage()
+}
  async function displayChatInterface(){
     let parameters ={
         method:"POST",
